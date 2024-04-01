836f0737dd24ec458515a6c4e7fe1e42003d4f4e
tranquangkha19 <tranquangkha478@gmail.com>
2024-04-01 14:08:55 +0700

    fix: fix right sidebar

--- lib/x_social_web/live/components/right_sidebar.ex
+++ lib/x_social_web/live/components/right_sidebar.ex
@@ -1,9 +1,23 @@
 defmodule XSocialWeb.RightSidebar do
   use Phoenix.LiveView
 
-  def render(assigns) do
+  def mount(_params, %{"user_id" => user_id}, socket) do
+    {:ok, assign(socket, :user_id, user_id)}
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def render(%{user_id: user_id} = assigns) do
     ~H"""
     <aside class="sidebar"></aside>
+    """
+  end
+
+  def render(assigns) do
+    ~H"""
+
     """
   end
 end
