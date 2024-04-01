43c81b7fff1546539ec430a38c56c5388f68bf1f
tranquangkha19 <tranquangkha478@gmail.com>
2024-04-01 11:26:06 +0700

    feat: logout icon

--- lib/x_social_web/live/header_live/header_live.ex
+++ lib/x_social_web/live/header_live/header_live.ex
@@ -24,10 +24,23 @@ defmodule XSocialWeb.HeaderLive do
           <input type="text" placeholder="Search" />
         </div>
         <div class="nav-items">
-          <a href="#" class="nav-item">Home</a>
-          <a href="#" class="nav-item">Notifications</a>
-          <a href="#" class="nav-item">Messages</a>
-          <a href="#" class="nav-item">Profile</a>
+          <a href="/logout" class="nav-item !ml-0">
+            <svg
+              class="h-7 w-7 text-black-500"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" />
+              <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+              <path d="M7 12h14l-3 -3m0 6l3 -3" />
+            </svg>
+          </a>
         </div>
       </nav>
     </header>
